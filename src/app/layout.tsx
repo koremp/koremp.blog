32e74f253832a73b365bfd7e75e89c27aab6177f
Dokyun Lim <im.koremp@gmail.com>
2024-06-14 17:53:59 +0900

    style: add font style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+
 import "./globals.css";
+
 import Footer from '../components/footer';
 import NavHeader from '../components/nav-header';
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "koremp.blog",
@@ -14,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="max-w-screen-lg min-w-[320px] mx-auto min-h-screen">
         <NavHeader />
         <main className="flex flex-col">
