7ff8c493e1cbfcbf22c05c7a1f16b0b2f3966979
Dokyun Lim <im.koremp@gmail.com>
2024-06-25 16:04:38 +0900

    refact: reset fs base function

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,6 +1,7 @@
+import { BedroomParentSharp } from '@mui/icons-material';
 import dayjs from 'dayjs';
-import { Dirent, readdirSync } from 'fs';
-import {sync} from 'glob'
+import { Dirent, existsSync, lstatSync, readFileSync, readdirSync } from 'fs';
+import { sync } from 'glob'
 import matter from 'gray-matter';
 import path from 'path';
 import readingTime from 'reading-time';
@@ -8,50 +9,80 @@ import readingTime from 'reading-time';
 const postDirectory = '/posts';
 const rootPostDirectory = path.join(process.cwd(), postDirectory);
 
-export const getUrl = (dirPath: string) => path.join('blog', dirPath);
+export const isPathDirectory = (givenPath: string) => existsSync(givenPath) && lstatSync(path.join(rootPostDirectory, givenPath)).isDirectory();
+export const isPathFile = (givenPath: string) => existsSync(givenPath) && lstatSync(path.join(rootPostDirectory, givenPath)).isFile()
 
-export const getDirInfo = (dirPath?: string) => {
-  const folder = (dirPath !== undefined)
-    ? path.join(rootPostDirectory, dirPath)
-    : rootPostDirectory;
-  console.log(folder)
-  const dirents: Dirent[] = readdirSync(folder, {withFileTypes : true});
-  const dirList = getDirList(dirents);
-  const postList = getPostList(dirents);
+// return dir-list, post-list and readme if readme exist
+export const getDirInfo = ({ givenPath }: { givenPath: string }) => {
+  const localPath = path.join(rootPostDirectory, givenPath);
+  console.log(localPath)
 
-  return {
-    dirList,
-    postList,
+  const dirents: Dirent[] = readdirSync(localPath, { withFileTypes: true });
+  const dirList = getDirListFromDirents({ dirents, givenPath });
+  const postList = getPostListFromDirents({ dirents, givenPath });
+
+  const readmeIndex = postList
+    .findIndex(post => post.name.toLowerCase() === 'readme.md');
+
+  console.log(givenPath)
+  // console.log(dirList, postList)
+
+  // if readme is exist
+  if(readmeIndex !== -1) {
+    const readme = [...postList][readmeIndex];
+    postList.splice(readmeIndex, 1);
+    console.log(readme)
+
+    return {
+      dirList,
+      postList,
+      readme: parsePost(path.join(readme.name))
+    }
   }
+
+  // if readme is undefined
+  return { dirList, postList, readme: undefined };
 }
 
-export interface IDirectory {
-  name: string
-  path: string
-  url: string
-  count: number
-}
-
-export const getDirList= (dirents: Dirent[]): IDirectory[] => {
-  return dirents
+export const getDirListFromDirents = ({ dirents, givenPath }: {dirents: Dirent[], givenPath: string}) => {
+  const dirList = dirents
     .filter(d => d.isDirectory())
     .map(d => {
-      const dirPath = path.join(rootPostDirectory, d.name)
+      const subPostCount = sync(`${rootPostDirectory}/${givenPath}/${d.name}/**/*.{md,mdx}`).length;
       return {
-         name: d.name,
-         path: dirPath,
-         url: getUrl(d.name),
-         count: getPostCount(dirPath)
+        name: d.name,
+        localPath:
+          givenPath === '/'
+            ? path.join(rootPostDirectory, d.parentPath)
+            : path.join(d.parentPath, d.name),
+        href: path.join(givenPath, d.name),
+        subPostCount
+      };
+    })
+    .filter(d => d.subPostCount !== 0);
+
+  return dirList;
+}
+
+export const getPostListFromDirents = ({ dirents, givenPath }:{dirents: Dirent[], givenPath: string}) => {
+  const postList = dirents
+    .filter(d => d.isFile())
+    .map(post => {
+      return {
+        name: post.name,
+        localPath: path.join(rootPostDirectory, post.parentPath),
+        href: path.join(givenPath, post.name),
       }
     });
+
+  return postList;
 }
 
-export const getPostList = (dirents: Dirent[]): string[] => {
-  return dirents
-    .filter(d => d.isFile())
-    .map(d => d.name);
-}
-
-export const getPostCount = (dirPath: string): number => {
-  return sync(`${dirPath}/**/*.{md,mdx}`).length;
+export const parsePost = (postPath: string) => {
+  const post = readFileSync(postPath, 'utf-8');
+  const { data, content } = matter(post);
+  const grayMatter = data;
+  const readingMinutes = Math.ceil(readingTime(content).minutes);
+  const dateString = dayjs(grayMatter.date).locale('ko').format('YYYY년 MM월 DD일');
+  return { ...grayMatter, dateString, content, readingMinutes };
 }
