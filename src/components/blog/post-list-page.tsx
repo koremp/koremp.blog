fc43903636db696b7db693fe2830da4f132d1834
Dokyun Lim <im.koremp@gmail.com>
2024-07-11 23:34:14 +0900

    refact: adjust post and dir page

--- src/components/blog/post-list-page.tsx
+++ src/components/blog/post-list-page.tsx
@@ -1,73 +1,62 @@
 import Link from 'next/link';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
-import { getDirInfo, isPathDirectory, isPathPost, parsePost } from '@/utils/fs'
+import { getDirectoryInfo } from '@/lib/file';
+import { parsePost } from '@/lib/post';
 
-export const BlogPage = (slug: string[]) => {
-  const path = slug.join('/')
-  if (isPathDirectory(path)) {
-    return <DirectoryPage slug={slug} />
-  }
-  else if (isPathPost(path)) {
-    return <PostPage slug={slug} />
-  }
-}
+export function DirectoryPage({ slug }: { slug: string[] }) {
+  const directoryInfo = getDirectoryInfo(slug);
 
-export const DirectoryPage = ({ slug }: { slug: string[] }) => {
-  if (isPathPost(slug.join('/'))) {
-    return <PostPage slug={slug} />
-  }
-
-  const { dirList, postList, post } = getDirInfo({ slug });
-  if (dirList === undefined) {
-    return <div>error</div>
-  }
+  const directoryList = directoryInfo.filter((item) => item.isDirectory);
+  const postList = directoryInfo.filter((item) => !item.isDirectory);
+  const readme = directoryInfo.find((item) => item.name === 'readme');
 
   return (
     <div>
-      {post && <PostPage slug={post.slug} />}
-      <div className='flex flex-col gap-2'>
-        {dirList.map(d => (
-          <Link href={d.href}>
-            <div>
+      <div>
+        Directories
+        <div>
+          {
+            directoryList.map(((directory) => (
               <div>
-                {d.name}
+                <div>{directory.name}</div>
+                <div>{directory.path}</div>
+                <div>{directory.href}</div>
               </div>
-              <div>
-                {d.localPath}
-              </div>
-              <div>
-                {d.href}
-              </div>
-              <div>
-                {d.subPostCount}
-              </div>
-            </div>
-          </Link>
-        ))}
+            )))
+          }
+        </div>
       </div>
-      <div className='flex flex-col gap-2'>
-        {postList.map(p => (
-          <Link href={p.href}>
-            <div>
-              {p.name}
-            </div>
-            <div>
-              {p.localPath}
-            </div>
-            <div>
-              {p.href}
-            </div>
-          </Link>
-        ))}
+      {
+        readme && (
+          <div>
+            {readme.name}
+            {readme.path}
+            {readme.href}
+          </div>
+        )
+      }
+      <div>
+        Posts
+        <div>
+          {
+            postList.map((post) => (
+              <div>
+                {post.name}
+                {post.path}
+                {post.href}
+              </div>
+            ))
+          }
+        </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
-export const PostPage = ({ slug }: { slug: string[] }) => {
+export function PostPage({ slug }: { slug: string[] }) {
   const {
-    dateString, content, readingMinutes
+    dateString, content, readingMinutes,
   } = parsePost(slug);
 
   return (
@@ -83,5 +72,5 @@ export const PostPage = ({ slug }: { slug: string[] }) => {
       </div>
     </div>
 
-  )
+  );
 }
